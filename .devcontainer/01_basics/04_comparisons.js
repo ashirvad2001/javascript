d383b7afa8c6b866021be557bfd442bf8c79019a
Ashirvad Kumar Singh <ashirvadkrs2001@gmail.com>
2025-06-16 10:24:02 +0000

    comparisons

---
+++ .devcontainer/01_basics/04_comparisons.js
@@ -0,0 +1,28 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2!=1);
+// console.log(2==1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+//in javascript comparison and equality check both works differently in null cases first two give false and in third it gives true because here null behave as zero
+
+// in case of undefined it gives the same result in all cases
+console.log(undefined==0);
+console.log(undefined>=0);
+console.log(undefined>0);
+//in most cases we avoid conversion 
+
+
+
+//  === (strict check -> it means it check strictly like not only value but also datatypes)
+console.log("2"===2);
+
