d5413f65a4888fc96fc609ff772453fb89c60a8f
Ashirvad Kumar Singh <ashirvadkrs2001@gmail.com>
2025-06-16 06:47:04 +0000

    data types

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,36 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3)  //we are using node js not browser
+
+console.log(3 +
+    3
+)  //code readability should be high
+
+console.log("hello")
+
+
+let name="Ashirvad"
+let age= 18
+let isLoggedIn = false
+
+// Primitive data types
+//number=> 2 to power 53
+// bigint
+// string => ""
+//boolean => true/false
+// null (is a standlone value) empty value
+// undefined => value not assigned
+// symbol => for unique value
+
+
+//object
+
+console.log(typeof "Ashirvad");
+console.log(typeof age);
+console.log(typeof undefined);     // undefined
+
+console.log(typeof null); // object
+
+
+
+
