0845dc458fdfeb942a200a303036d2e250d9ddef
Ashirvad Kumar Singh <ashirvadkrs2001@gmail.com>
2025-06-16 07:16:04 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,47 @@
+// let score =  "33"
+// let score =  "33abc"
+// let score =  null
+// let score =  undefined
+// let score =  true
+let score =  "ashirvad"
+
+
+
+
+
+ console.log(typeof score);
+ console.log(typeof (score));
+ 
+ let valueInNumber = Number(score)
+ console.log(typeof valueInNumber);
+ console.log(valueInNumber);
+
+
+ //  "33" => converted to 33
+//  "33abc" => its value is NaN but type is a number
+// true=> 1 and false => 0
+
+
+
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+ let isLoggedIn = "ashirvad"
+
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => gives true and 0=> gives false
+//  "" = false
+// "ashirvad" gives true value
+
+
+
+let someNumber = 45
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
