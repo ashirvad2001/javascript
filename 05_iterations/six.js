95a27cf881d776318ce2e0745061653adf7530d2
Ashirvad Kumar Singh <ashirvadkrs2001@gmail.com>
2025-07-16 09:30:01 +0000

    filter

---
+++ 05_iterations/six.js
@@ -0,0 +1,35 @@
+// Filter map and reduce
+
+// const coding = ["HTML", "CSS", "Javascript", "Java", "Python", "React"]
+
+// //so foreach doesnot give return value here it's output becomes undefined 
+
+// const values = coding.forEach( (item) => {
+//     //console.log(item);
+//     return item
+    
+// } )
+
+// console.log(values);
+
+
+
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNums.filter( (num) =>{
+//     return num>4
+// })
+
+//**************************************By applying foreach method***************************** */
+const newNums = []
+myNums.forEach( (num) => {
+    if(num>4){
+        newNums.push(num)
+    }
+})
+
+console.log(newNums);
+
+
+//some practice can be done by watching video
